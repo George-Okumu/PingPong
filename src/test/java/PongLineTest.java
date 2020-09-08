3d63c7e9dea5a6b375bd2a0e46a863b7f14692b3
George Okumu <gokumu12@gmail.com>
2020-09-08 06:19:58 +0300

    Created another class, and run tests for instantiation

---
+++ src/test/java/PongLineTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PongLineTest {
+    @Test
+    public void newPongLine_instantiatesCorrectly() {
+        PongLine testPongLine = new PongLine();
+        Assert.assertTrue(true);
+    }
+}
